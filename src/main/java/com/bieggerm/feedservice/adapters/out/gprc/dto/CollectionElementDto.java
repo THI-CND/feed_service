60809eea740014138cff5a1957d766c7db2f7db9
Marius Biegger <biegger.marius@gmail.com>
2025-01-10 22:06:00 +0100

    updated mvn for grpc

---
+++ src/main/java/com/bieggerm/feedservice/adapters/out/gprc/dto/CollectionElementDto.java
@@ -0,0 +1,38 @@
+package com.bieggerm.feedservice.adapters.out.gprc.dto;
+
+import com.bieggerm.feedservice.domain.model.CollectionElement;
+import com.bieggerm.feedservice.domain.model.FeedElement;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@Data
+public class CollectionElementDto implements Serializable {
+    private String author;
+    private final String type = "collection";
+    private String id;
+    private String name;
+    private String description;
+
+    public static CollectionElementDto fromElementToDto(CollectionElement collectionElement) {
+        CollectionElementDto entity = new CollectionElementDto();
+        entity.setName(collectionElement.getName());
+        entity.setDescription(collectionElement.getDescription());
+        entity.setAuthor(collectionElement.getAuthor());
+        entity.setId(collectionElement.getId());
+        return entity;
+    }
+
+    public FeedElement fromDtoToElement(CollectionElementDto collectionElementDto) {
+        CollectionElement collectionElement = new CollectionElement();
+        collectionElement.setName(collectionElementDto.getName());
+        collectionElement.setDescription(collectionElementDto.getDescription());
+        collectionElement.setAuthor(collectionElementDto.getAuthor());
+        collectionElement.setId(collectionElementDto.getId());
+        return collectionElement;
+    }
+}
--- src/main/java/com/bieggerm/feedservice/adapters/out/rest/RecipeProviderImpl.java
+++ src/main/java/com/bieggerm/feedservice/adapters/out/rest/RecipeProviderImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
 @Service
 public class RecipeProviderImpl implements RecipeProvider {
 
